7571d45dbf9e00e8dbfe3c857cb48a4cd84febe8
Hayley Wood <onlinecowork@gmail.com>
2022-05-10 20:55:47 +1000

    create slider, edit css

---
+++ reviews-slider/src/App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import reviewData from "./reviewData.js";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+export default function App() {
+  const [slideIndex, setSlideIndex] = React.useState(0);
+  const { name, job, description, image } = reviewData[slideIndex];
+
+  function handleBack() {
+    if (slideIndex > 0) {
+      setSlideIndex((prevIndex) => prevIndex - 1);
+    } else if (slideIndex === 0) {
+      setSlideIndex((prevIndex) => (prevIndex = reviewData.length - 1));
+    }
+  }
+  function handleForward() {
+    if (slideIndex < reviewData.length - 1) {
+      setSlideIndex((prevIndex) => prevIndex + 1);
+    } else if (slideIndex === reviewData.length - 1) {
+      setSlideIndex(0);
+    }
+  }
+  function handleSurpriseMe() {
+    setSlideIndex(
+      (prevIndex) => (prevIndex = Math.floor(Math.random() * reviewData.length))
+    );
+  }
+
+  return (
+    <div className='text-center'>
+      <header className='header'>
+        <h1 className='header--title'>Our Reviews</h1>
+        <hr className='header--underline' />
+      </header>
+      <main className='main container flex column align-center'>
+        <article className='main--person'>
+          <div className='main--person--image_div'>
+            <img className='main--person--image' src={image} alt={name} />
+            <FaQuoteRight className='quote-icon' />
+          </div>
+          <h2 className='main--person--name'>{name}</h2>
+          <h3 className='main--person--job'>{job}</h3>
+          <p className='main--person--description'>{description}</p>
+        </article>
+        <div className='main--buttons'>
+          <button className='main--buttons--back' onClick={handleBack}>
+            <FaChevronLeft />
+          </button>
+          <button className='main--buttons--forward' onClick={handleForward}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className='main--button--surprise' onClick={handleSurpriseMe}>
+          Surprise me
+        </button>
+      </main>
+    </div>
+  );
+}
---
+++ reviews-slider/src/reviewData.js
@@ -0,0 +1,40 @@
+const reviewData = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    name: "John James",
+    job: "Web developer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sed vitae impedit nisi asperiores odit officia ex, accusamus itaque sapiente enim quae repellendus provident? Harum distinctio eos alias cum placeat?",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Susan Smith",
+    job: "UX designer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sed vitae impedit nisi asperiores odit officia ex, accusamus itaque sapiente enim quae repellendus provident? Harum distinctio eos alias cum placeat?",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    name: "Anna Johnson",
+    job: "Digital Marketer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sed vitae impedit nisi asperiores odit officia ex, accusamus itaque sapiente enim quae repellendus provident? Harum distinctio eos alias cum placeat?",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "Peter Jones",
+    job: "Intern",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sed vitae impedit nisi asperiores odit officia ex, accusamus itaque sapiente enim quae repellendus provident? Harum distinctio eos alias cum placeat?",
+  },
+];
+
+export default reviewData;
